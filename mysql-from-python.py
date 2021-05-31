01edc8b641f08c7993b58fe8de89b722d2bfb1a5
kylelawrencedev <kylelawrence19@gmail.com>
2021-05-31 18:23:40 +0000

    Python and MySQL - Delete Lesson

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 import pymysql
 
 # Get the username from the Cloud9 workspace
@@ -11,11 +10,13 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-
 try:
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE Friends SET age = %s WHERE name = %s;",
-                        (23, 'Bob'))
+        rows = [(23, 'bob'),
+                (24, 'jim'),
+                (25, 'fred')]
+        cursor.executemany("UPDATE Friends SET age = %s WHERE name = %s;",
+                           rows)
         connection.commit()
 finally:
     connection.close()

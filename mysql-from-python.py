626ae2d3aa2d13c2752e64be90adf34e1a7ce25f
kylelawrencedev <kylelawrence19@gmail.com>
2021-05-31 18:35:08 +0000

    Python & MySQL - Execute Many Lesson Update

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,5 +1,5 @@
-  
 import os
+import datetime
 import pymysql
 
 # Get the username from the Cloud9 workspace
@@ -14,7 +14,10 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        rows = cursor.executemany("DELETE FROM Friends WHERE name = %s;", ['bob','jim','fred'])
+        rows = [("bob", 21, "1990-02-06 23:04:56"),
+                ("jim", 56, "1955-05-09 13:12:45"),
+                ("fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
         connection.commit()
 finally:
     connection.close()
